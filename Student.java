e4f64996dc418411fce3cfc0d5a57e95468094bf
Flopipc <fs.christiaansen@student.avans.nl>
2022-12-16 13:51:41 +0100

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,71 @@
+public class Student {
+
+    private String email;
+    private String name;
+    private String birthday;
+    private String sex;
+    private String adress;
+    private String residence;
+    private String country;
+
+    public Student(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return this.birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAdress() {
+        return this.adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getResidence() {
+        return this.residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
