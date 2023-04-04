5f533d01dde118fe92bea062e500b8c51b3216e3
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-04-04 17:51:59 +0200

    [CREATE]
    created an application controller from where we can control, and add different scenes from.

---
+++ src/GUI/ApplicationController.java
@@ -0,0 +1,40 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+
+
+
+
+public class ApplicationController extends Application {
+    Scene index;
+
+    Stage window;
+
+    @Override
+    public void start(Stage window) throws Exception {
+        this.window = window;
+        BorderPane layout = new BorderPane();
+
+        VBox list = new VBox();
+
+        list.getChildren().add(new Label("Group 12"));
+        list.getChildren().add(new Label("Ivan van Dijk & Tido Koldenhof"));
+        HBox buttons = new HBox();
+        Button Courses = new Button("Test Button");
+       
+        buttons.getChildren().addAll(Courses);
+        list.getChildren().add(buttons);
+        layout.setCenter(list);
+        this.index = new Scene(layout);
+        window.setScene(index);
+        window.show();
+    }
+
+}
