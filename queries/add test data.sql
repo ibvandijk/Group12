76b9107c2e130b085e483b8f6c1c4826ec085601
Tido <tido.koldenhof@gmail.com>
2023-04-12 14:29:17 +0200

    [CREATE] Queries for creating and adding test data

---
+++ queries/add test data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Course (CourseName, Subject, IntroductionText, Difficulty)
+VALUES ('TestCourse', 'Testing', 'You will be testing the database', 1)
+
+INSERT INTO Student (Email, Name, Birthday, Sex, Adress, Residence, Country)
+VALUES ('j.arbuckle@student.avans.nl', 'Jon Arbuckle', convert(date,'28/07/1951', 103), 'No', '711 Maple Street', '', 'United States')
