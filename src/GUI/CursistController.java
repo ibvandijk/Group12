b545098a0794dc6048a004895603d2713298e73d
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-04-15 12:01:34 +0200

    [UPDATE] added add button functionality

--- src/GUI/CursistController.java
+++ src/GUI/CursistController.java
@@ -5,16 +5,21 @@ import Datastorage.DatabaseConnection;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TextField;
 
-import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,6 +27,8 @@ import java.sql.Date;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+
 
 
 public class CursistController extends Application {
@@ -55,7 +62,7 @@ public class CursistController extends Application {
 
     public Scene Cursists(){
         BorderPane layout = new BorderPane();
-        Scene cursists = new Scene(layout);
+        Scene cursists = new Scene(layout, 500,500);
 
         try {
             ResultSet resultSet = dbConnection.executeSQLSelectStatement("SELECT * FROM Student");
@@ -74,7 +81,7 @@ public class CursistController extends Application {
             }
 
 
-            layout.setLeft(table);
+            layout.setCenter(table);
 
             Button add = new Button("Add");
             Button delete = new Button("Delete");
@@ -83,6 +90,13 @@ public class CursistController extends Application {
             HBox buttons = new HBox();
             buttons.getChildren().addAll(Back, add, view, delete);
             layout.setTop(buttons);
+
+            add.setOnAction((EventHandler) -> {
+                Stage stage = new Stage();
+                stage.setScene(addCursist());
+                stage.show();
+
+            });
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -94,4 +108,56 @@ public class CursistController extends Application {
     @Override
     public void start(Stage stage) throws Exception {
     }
+
+    public Scene addCursist(){
+        GridPane grid = new GridPane();
+
+        Text scenetitle = new Text("Nieuw Student toevoegen:");
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+
+        // email input form
+        Label email = new Label("E-mail Student:");
+        grid.add(email, 0, 1);
+        TextField emailTextField = new TextField();
+        grid.add(emailTextField, 1, 1);
+
+        // naam input form
+        Label name = new Label("Naam Student:");
+        grid.add(name, 0, 2);
+        TextField nameTextField = new TextField();
+        grid.add(nameTextField, 1, 2);
+
+        // geboortedatum input form
+        Label birthDay = new Label("geboortedatum Student: ");
+        grid.add(birthDay, 0, 3);
+        TextField birthDayTextField = new TextField();
+        grid.add(birthDayTextField, 1, 3);
+
+        // sex input form
+        Label Sex = new Label("Sex Student: ");
+        grid.add(Sex, 0, 4);
+        TextField SexTextField = new TextField();
+        grid.add(SexTextField, 1, 4);
+
+        // adress input form
+        Label adress = new Label("adress Student: ");
+        grid.add(adress, 0, 5);
+        TextField adressTextField = new TextField();
+        grid.add(adressTextField, 1, 5);
+
+        // country input form
+        Label country = new Label("country Student: ");
+        grid.add(country, 0, 6);
+        TextField countryTextField = new TextField();
+        grid.add(countryTextField, 1, 6);
+
+        
+        
+
+        return new Scene(grid);
+    }
+
 }
+
+
